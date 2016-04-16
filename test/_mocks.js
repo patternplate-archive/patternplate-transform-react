a6b04d3ae59c092380fb4c62f018a731bfabac5f
Mario Nebl <hello@herebecode.com>
2016-04-16 14:22:12 +0200

    test(system): beef up test suite

--- test/_mocks.js
+++ test/_mocks.js
@@ -73,6 +73,8 @@ export const plainAsiFile = {
 export const statelessFile = {
 	...file,
 	buffer: new Buffer(unindent(`
+	import React from 'react';
+
 	export default (props) => {
 		return (<div />);
 	};
@@ -93,5 +95,96 @@ export const fullFile = {
 	}
 	`)),
 	path: 'full/index.jsx',
+	dependencies: {}
+};
+
+export const reservedPropsDeclaration = {
+	...file,
+	buffer: new Buffer(unindent(`
+	const props = {
+		content: 'foo',
+		className: 'bar'
+	};
+	<div {...this.props} className={props.className}>
+		{props.content}
+	</div>
+	`)),
+	dependencies: {}
+};
+
+export const reservedContextDeclaration = {
+	...file,
+	buffer: new Buffer(unindent(`
+	const context = {
+		content: 'foo',
+		className: 'bar'
+	};
+	<div {...this.props} className={context.className}>
+		{context.content}
+	</div>
+	`)),
+	dependencies: {}
+};
+
+export const variableDeclarator = {
+	...file,
+	buffer: new Buffer(unindent(`
+	const Test = {};
+	const key = props.foo;
+	<div {...props} key={key}/>
+	`)),
+	dependencies: {}
+};
+
+export const functionDeclarator = {
+	...file,
+	buffer: new Buffer(unindent(`
+	// function Test(props) { console.log(props); };
+	function _Test() { console.log(props); };
+	// Test(props);
+	_Test();
+	<div/>
+	`)),
+	dependencies: {}
+};
+
+export const classDeclarator = {
+	...file,
+	buffer: new Buffer(unindent(`
+	class Test {
+		render() {
+			const {
+				component: Component,
+				...props
+			} = this.props;
+			return <Component {...props} />;
+		}
+	};
+	new Test();
+	<div/>
+	`)),
+	dependencies: {}
+};
+
+export const plainThis = {
+	...file,
+	buffer: new Buffer(unindent(`
+	const foo = this.props.foo;
+	class Foo extends React.Component {
+		render() {
+			return (
+				<div {...this.props}>
+					{this.props.children}
+				</div>
+			);
+		}
+	}
+	<div {...this.props}>
+		{this.props.children}
+		<Foo {...foo}>
+			{foo.children}
+		</Foo>
+	</div>
+	`)),
 	dependencies: {}
 };
--- test/index.js
+++ test/index.js
@@ -3,6 +3,7 @@ import 'babel-polyfill';
 import test from 'ava';
 
 import factory from '../source';
+import {virtualModule, virtualRender} from './_helpers';
 import * as mocks from './_mocks';
 
 test.beforeEach(t => {
@@ -41,29 +42,153 @@ test('the resolved object should havea a buffer key', async t => {
 	t.truthy(file.hasOwnProperty('buffer'));
 });
 
-test('when transforming plain jsx', t => {
+test('when transforming plain jsx', async t => {
 	const {context: {transform}} = t;
-	const execution = transform(mocks.plainFile);
-	t.notThrows(execution, ', it should not fail');
+	const result = await transform(mocks.plainFile);
+
+	{
+		const Actual = virtualModule(result.buffer);
+		t.falsy(new Actual() instanceof Actual, 'it should return a function');
+	}
+
+	{
+		const actual = virtualRender(result.buffer);
+		const expected = '<div></div>';
+		t.is(actual, expected, 'it should yield the expected render output');
+	}
 });
 
-test('when transforming a react stateless component', t => {
+test('when transforming a react stateless component', async t => {
 	const {context: {transform}} = t;
-	const execution = transform(mocks.statelessFile);
-	t.notThrows(execution, 'it should not fail');
+	const result = await transform(mocks.statelessFile);
+
+	{
+		const Actual = virtualModule(result.buffer);
+		t.falsy(new Actual() instanceof Actual, 'it should return a function');
+	}
+
+	{
+		const actual = virtualRender(result.buffer);
+		const expected = '<div></div>';
+		t.is(actual, expected, 'it should yield the same render output');
+	}
 });
 
-test('when transforming a react class declaration', t => {
+test('when transforming a react class declaration', async t => {
 	const {context: {transform}} = t;
-	const execution = transform(mocks.fullFile);
-	t.notThrows(execution, 'a react class definition, it should not fail');
+	const result = await transform(mocks.fullFile);
+
+	{
+		const Actual = virtualModule(result.buffer);
+		t.truthy(new Actual() instanceof Actual, 'it should return a class');
+	}
+
+	{
+		const actual = virtualRender(result.buffer);
+		const expected = '<div></div>';
+		t.is(actual, expected, 'it should yield the same render output');
+	}
 });
 
 /* This fails with an uncaught exception, which basically is what we want here
-TODO: wrap your head around this
+TODO: wrap your head around why this happens
 
 test('when transforming invalid plain jsx', async t => {
 	const {context: {transform}} = t;
 	const execution = transform(mocks.plainAsiFile);
 	await t.throws(execution, 'it should fail');
 }); */
+
+test(
+	'when transforming plain jsx with variable declaration "props"',
+	async t => {
+		const {context: {transform}} = t;
+		const result = await transform(mocks.reservedPropsDeclaration);
+
+		{
+			const Actual = virtualModule(result.buffer);
+			t.truthy(new Actual() instanceof Actual, 'it should return a class');
+		}
+
+		{
+			const props = {id: 'foo', className: 'baz'};
+			const actual = virtualRender(result.buffer, {}, props);
+			const expected = '<div id="foo" class="bar">foo</div>';
+			t.is(actual, expected, 'it should yield expected render output');
+		}
+	}
+);
+
+test(
+	'when transforming plain jsx with variable declaration "context"',
+	async t => {
+		const {context: {transform}} = t;
+		const result = await transform(mocks.reservedContextDeclaration);
+
+		{
+			const Actual = virtualModule(result.buffer);
+			t.truthy(new Actual() instanceof Actual, 'it should return a class');
+		}
+
+		{
+			const props = {id: 'foo', className: 'baz'};
+			const actual = virtualRender(result.buffer, {}, props);
+			const expected = '<div id="foo" class="bar">foo</div>';
+			t.is(actual, expected, 'it should yield expected render output');
+		}
+	}
+);
+
+test(
+	'when transforming plain jsx with member expressions to "this"',
+	async t => {
+		const {context: {transform}} = t;
+		const result = await transform(mocks.plainThis);
+
+		{
+			const props = {
+				foo: {id: 'bar', children: 'bar'},
+				id: 'foo', children: 'foo'
+			};
+			const actual = virtualRender(result.buffer, {}, props, 'foo');
+			const expected = '<div id="foo">foo<div id="bar">bar</div></div>';
+			t.is(actual, expected, 'it should yield expected render output');
+		}
+	}
+);
+
+test(
+	[
+		'when transforming plain jsx with a conflicting variable declarator to a',
+		'stateless component'
+	].join(' '),
+	t => {
+		const {context: {transform}} = t;
+		const execution = transform(mocks.variableDeclarator);
+		t.notThrows(execution, 'it should not fail');
+	}
+);
+
+test(
+	[
+		'when transforming plain jsx with a conflicting function declarator to a',
+		'stateless component'
+	].join(' '),
+	t => {
+		const {context: {transform}} = t;
+		const execution = transform(mocks.functionDeclarator);
+		t.notThrows(execution, 'it should not fail');
+	}
+);
+
+test(
+	[
+		'when transforming plain jsx with a conflicting class declarator to a',
+		'stateless component'
+	].join(' '),
+	t => {
+		const {context: {transform}} = t;
+		const execution = transform(mocks.classDeclarator);
+		t.notThrows(execution, 'it should not fail');
+	}
+);
