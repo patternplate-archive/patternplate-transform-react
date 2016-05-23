0c3852d85e43716cc909e3b2bf2a0a7b47aefedd
Mario Nebl <hello@herebecode.com>
2016-05-23 14:03:10 +0200

    fix(generation): merge node globals into injection

--- source/injection-template.js
+++ source/injection-template.js
@@ -2,7 +2,7 @@ import template from 'babel-template';
 
 export default template(`
 	INJECTION;
-	var global = Object.assign({}, IDENTIFIER);
+	var global = Object.assign({}, global, IDENTIFIER);
 	`,
 	{
 		sourceType: 'module'
