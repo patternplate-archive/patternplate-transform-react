6ccfb8a251a3eb198e4d26be35a68c245b1b932a
Mario Nebl <marionebl@users.noreply.github.com>
2016-12-15 12:06:55 +0100

    Fix/avoid object assign (#97)
    * chore: sanitize test setup
    
    * test: add basic test case for globals
    
    * fix: avoid object.assign to ensure browser compat

--- source/injection-template.js
+++ source/injection-template.js
@@ -2,11 +2,15 @@ import template from 'babel-template';
 
 export default template(`
 	INJECTION;
-	var global = Object.assign({}, global, IDENTIFIER);
+	var global = {};
+
+	for (var key in IDENTIFIER) {
+		if ({}.hasOwnProperty.call(IDENTIFIER, key)) {
+			global[key] = IDENTIFIER[key];
+		}
+	}
 	`,
 	{
 		sourceType: 'module'
 	}
 );
-
-
